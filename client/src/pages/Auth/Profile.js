0d043f69e5fdc74e7dabdc45d97d0b5cd44767ee
Ajit Fawade <ajitfawade@gmail.com>
2020-08-05 23:53:16 +0530

    Populated user detais on profile page

--- client/src/pages/Auth/Profile.js
+++ client/src/pages/Auth/Profile.js
@@ -1,7 +1,126 @@
-import React from "react";
+import React, { useState, useMemo, useContext } from "react";
+import { toast } from "react-toastify";
+import { gql } from "apollo-boost";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+
+const PROFILE = gql`
+  query {
+    profile {
+      _id
+      name
+      about
+      username
+      email
+      images {
+        url
+        public_id
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 const Profile = () => {
-  return <h4>Profile</h4>;
+  const [values, setValues] = useState({
+    username: "",
+    name: "",
+    email: "",
+    about: "",
+    images: [],
+  });
+  const { name, username, email, about, images } = values;
+  const [loading, setLoading] = useState(false);
+  const { data } = useQuery(PROFILE);
+  useMemo(() => {
+    if (data) {
+      console.log("PROFILE:", data.profile);
+      setValues({
+        username: data.profile.username,
+        name: data.profile.name,
+        email: data.profile.email,
+        about: data.profile.about,
+        images: data.profile.images,
+      });
+    }
+  }, [data]);
+
+  const handleSubmit = () => {};
+
+  const handleImageChange = () => {};
+
+  const handleChange = () => {};
+
+  const profileUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Username</label>
+        <input
+          className="form-control"
+          placeholder="Username"
+          disabled={loading}
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          className="form-control"
+          placeholder="Name"
+          disabled={loading}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          className="form-control"
+          placeholder="Email"
+          disabled={loading}
+          type="email"
+          name="email"
+          disabled
+          value={email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Image</label>
+        <input
+          className="form-control"
+          accept="image/*"
+          placeholder="Image"
+          type="file"
+          onChange={handleImageChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>About</label>
+        <textarea
+          className="form-control"
+          placeholder="About"
+          disabled={loading}
+          name="about"
+          value={about}
+          onChange={handleChange}
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={!email || loading}
+        className="btn btn-primary"
+      >
+        Submit
+      </button>
+    </form>
+  );
+  return <div className="container">{profileUpdateForm()}</div>;
 };
 
 export default Profile;
